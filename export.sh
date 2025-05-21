3bd321ad2c1a6afeb4e4091941490e41d71410e2
Oskar Codes <oskarbertrand@icloud.com>
2025-05-21 19:04:10 +0200

    Create export script

---
+++ export.sh
@@ -0,0 +1,21 @@
+
+
+# If directory dist exists, remove it
+if [ -d "dist" ]; then
+    rm -rf dist
+fi
+
+# Create directory dist
+mkdir dist
+
+# Create directoy dist/build
+mkdir dist/build
+
+# Copy index.html to dist
+cp index.html dist/index.html
+
+# Copy ./resources to dist
+cp -r ./resources dist/resources
+
+# Copy ./build/* to dist/build
+cp -r ./build/* dist/build
